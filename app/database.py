4320d8a021baca0e323ab45a5f3aebe3eb342080
Tomas <tomasbalbinder@gmail.com>
2023-10-12 10:26:59 +0200

    final

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Idefixa@localhost:5432/Cosmonauts_databaze"
+SQLALCHEMY_DATABASE_URL = "postgresql://name:password@localhost:5432/Cosmonauts_databaze"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
